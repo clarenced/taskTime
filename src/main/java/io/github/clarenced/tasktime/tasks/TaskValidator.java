c8b8a05cda10b9b35f9b2968ffe9180ca6cccf61
Clarence Charles <clarence.dimitri.charles@gmail.com>
2025-05-01 12:54:05 +0200

    Refactor task validation logic with constant lengths.
    Introduced constants for title and description length limits to improve readability and maintainability. Updated validation checks to use these newly defined constants, ensuring consistent and centralized enforcement of length constraints.

--- src/main/java/io/github/clarenced/tasktime/tasks/TaskValidator.java
+++ src/main/java/io/github/clarenced/tasktime/tasks/TaskValidator.java
@@ -5,10 +5,15 @@ import io.github.clarenced.tasktime.common.Result;
 public class TaskValidator {
 
 
+    public static final int TITLE_MINIMAL_LENGTH = 5;
+    public static final int TITLE_MAXIMUM_LENGTH = 30;
+    public static final int DESCRIPTION_MAXIMUM_LENGTH = 300;
+    public static final int DESCRIPTION_MINIMUM_LENGTH = 5;
+
     /**
      * Validates a task creation request.
      *
-     * @param createTaskDto The task creation request to validate
+     * @param createTaskDto The task creation request to validateŒ
      * @return A Result containing either a validated CreateTaskDto or an ErrorDto
      */
     public static Result<TaskTimeApi.CreateTaskDto, TaskTimeApi.ErrorDto> validateTask(TaskTimeApi.CreateTaskDto createTaskDto) {
@@ -18,16 +23,16 @@ public class TaskValidator {
         if (createTaskDto.description().isEmpty()) {
             return Result.error(ErrorFactory.descriptionIsEmpty());
         }
-        if(createTaskDto.title().length() <= 5) {
+        if(createTaskDto.title().length() <= TITLE_MINIMAL_LENGTH) {
             return Result.error(ErrorFactory.titleHasLessThan5Characters());
         }
-        if(createTaskDto.title().length() > 30) {
+        if(createTaskDto.title().length() > TITLE_MAXIMUM_LENGTH) {
             return Result.error(ErrorFactory.titleHasMoreThan30Characters());
         }
-        if(createTaskDto.description().length() > 300) {
+        if(createTaskDto.description().length() > DESCRIPTION_MAXIMUM_LENGTH) {
             return Result.error(ErrorFactory.descriptionHasMore300Characters());
         }
-        if(createTaskDto.description().length() <= 5) {
+        if(createTaskDto.description().length() <= DESCRIPTION_MINIMUM_LENGTH) {
             return Result.error(ErrorFactory.descriptionHasLessThan5Characters());
         }
         return Result.success(createTaskDto);
