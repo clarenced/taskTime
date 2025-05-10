30756284a2fefe8e9b50456ff6be8e7402eb4e7d
Clarence Charles <clarence.dimitri.charles@gmail.com>
2025-05-10 10:37:22 +0200

    Add Flyway migration and PostgreSQL integration
    * Introduced Flyway for database version control and created initial migrations to define the `task` table and add an index on `status`.
    * Configured PostgreSQL as the datasource and updated dependencies to include Flyway, PostgreSQL, and TestContainers for integration tests.
    * Added `application.yml` for datasource and JPA configurations.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.3"
+    id("org.flywaydb.flyway") version "9.22.3"
 }
 
 group = "io.github.clarenced"
@@ -15,6 +16,12 @@ dependencies {
     // Spring Boot starters
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jpa")
+
+    // PostgreSQL et Flyway
+    implementation("org.postgresql:postgresql:42.7.1")
+    implementation("org.flywaydb:flyway-core")
+
 
     // Spring Boot DevTools for development
     implementation("org.springframework.boot:spring-boot-devtools")
@@ -23,6 +30,13 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // TestContainers
+    testImplementation(platform("org.testcontainers:testcontainers-bom:1.19.3"))
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:testcontainers")
+
 }
 
 tasks.test {
---
+++ src/main/resources/db/migration/V1__Create_task_table.sql .sql
@@ -0,0 +1,14 @@
+-- V1__Create_task_table.sql
+CREATE TYPE task_status AS ENUM ('TO_DO', 'IN_PROGRESS', 'DONE');
+
+CREATE TABLE task (
+  id BIGSERIAL PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  description VARCHAR(300) NOT NULL,
+  status task_status NOT NULL,
+
+  CONSTRAINT check_title_not_empty CHECK (LENGTH(TRIM(title)) > 0),
+  CONSTRAINT check_title_length CHECK (LENGTH(TRIM(title)) BETWEEN 5 AND 30),
+  CONSTRAINT check_description_not_empty CHECK (LENGTH(TRIM(description)) > 0),
+  CONSTRAINT check_description_length CHECK (LENGTH(TRIM(description)) BETWEEN 5 AND 300)
+);
