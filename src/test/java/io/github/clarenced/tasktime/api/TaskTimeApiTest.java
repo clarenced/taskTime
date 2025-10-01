b86ec8d9aad7e1cbab481cce2dcbed02c2bee321
Clarence Charles <clarence.dimitri.charles@gmail.com>
2025-10-01 07:11:09 +0200

    Cleanup

--- src/test/java/io/github/clarenced/tasktime/api/TaskTimeApiTest.java
+++ src/test/java/io/github/clarenced/tasktime/api/TaskTimeApiTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
